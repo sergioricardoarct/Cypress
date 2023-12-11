21e9838a3fcc283b4744c7f39a98730eb8c35fe9
sergio <sergioricardo.arct@gmail.com>
2023-12-11 13:37:21 -0300

    cypress 11-12-2023 1337

---
+++ cypress/e2e/Testes_Cypress/003_Textos.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+
+describe ('teste_Texto', () => {
+    before (() => {
+         cy.visit("https://demo.guru99.com/telecom/index.html")})
+    
+    it("Busca do titulo", ()=>{
+        cy.get('.left > .logo').should("be.visible")
+    })
+    it.only("Busca do Add Costumer", ()=>{
+        cy.get('.inner > .left > :nth-child(1)')
+        .should('contain','Add Customer')
+        cy.get('.left > :nth-child(1) > h3 > a ')
+        .click()
+        cy.get('h1').should("be.visible")
+    })
+    
+
+}) 
