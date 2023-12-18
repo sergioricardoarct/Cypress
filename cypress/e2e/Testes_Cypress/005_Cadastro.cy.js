8c92a4c5f41ca9501004f914df3edfd35bc28df6
sergio <sergioricardo.arct@gmail.com>
2023-12-18 12:47:33 -0300

    Cypress 18-12-2023 1247

---
+++ cypress/e2e/Testes_Cypress/005_Cadastro.cy.js
@@ -0,0 +1,73 @@
+/// <reference types="cypress" />
+
+
+describe ('Teste_Orange', () => {
+    beforeEach(() => {
+         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+                
+        })
+         
+    it("pagina aberta", ()=>{
+
+        cy.get('.orangehrm-login-logo > img').should("be.visible")
+    }) 
+
+    
+    it("Login", ()=>{
+
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').click()
+
+        cy.get('.oxd-brand-banner > img').should("be.visible")
+    }) 
+
+    it("Clicar em pim", ()=>{
+
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').click()
+
+        cy.get(':nth-child(2) > .oxd-main-menu-item > .oxd-text').click()
+    })
+    it.only('Cadastrar',()=>{
+        ///Login///
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').click()
+
+        cy.get('.oxd-brand-banner > img').should("be.visible")
+
+        ///Clicar no pim///
+         cy.get(':nth-child(2) > .oxd-main-menu-item > .oxd-text').click()
+
+        ///cadastro///
+        cy.get('.orangehrm-header-container > .oxd-button').click()
+        cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type("Amentep")
+        cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type("Hapinatra")
+        cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type("II")
+
+        let Idemployee = "0001"
+        cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input')
+        .click().clear().type(Idemployee)
+        
+
+        cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input')            
+        cy.get('.oxd-button--secondary').click()
+        cy.get('.oxd-toast').should('be.visible')
+
+        
+        cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-item').click()
+        cy.get(':nth-child(2) > .oxd-input').type(Idemployee)
+        cy.get('.oxd-form-actions > .oxd-button--secondary').click()
+
+        cy.get('.orangehrm-horizontal-padding > .oxd-text'). should('contain','(1) Record Found')
+
+        /// delet
+
+        cy.get('.oxd-table-cell-actions > :nth-child(1) > .oxd-icon').click()
+        cy.get('.oxd-button--label-danger').click()
+        cy.get('.oxd-toast').should('be.visible')
+            
+    })      
+})
