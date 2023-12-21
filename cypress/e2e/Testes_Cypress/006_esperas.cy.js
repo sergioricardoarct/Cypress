a9dd66bf019b4a34e6defdc27d29568b3c37f82a
sergio <sergioricardo.arct@gmail.com>
2023-12-21 14:00:52 -0300

    Cypress 21-12-203 1400

--- cypress/e2e/Testes_Cypress/006_esperas.cy.js
+++ cypress/e2e/Testes_Cypress/006_esperas.cy.js
@@ -5,11 +5,17 @@ describe ('teste_Texto', () => {
     beforeEach (() => {
          cy.visit("https://wcaquino.me/cypress/componentes.html")})
 
-    it ("Combo Multiplos", ()=>{
+    it ("Delay", ()=>{
         cy.get('#buttonDelay').click()
         
         cy.get('#novoCampo').should('be.visible')
 
     })
 
+    it ("Wait...", ()=>{
+        cy.get('#buttonDelay').click()
+        .wait(4000)                                 // em milisegundos
+        cy.get('#novoCampo').should('be.visible')
+
+    })  
 })
